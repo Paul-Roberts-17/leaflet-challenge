a3aedca534a9ba4857b1b3f6d68cf97509699ecd
Paul-Roberts-17 <merovingi@hotmail.com>
2023-03-31 21:19:58 +0800

    marker circle radius based on magnitude

--- Leaflet-part-1/static/js/logic.js
+++ Leaflet-part-1/static/js/logic.js
@@ -15,17 +15,21 @@
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(myMap);
+
+    
+
     // Loop through the data.
     for (let i = 0; i < data.features.length; i++) {
 
     // Set the data location property to a variable.
         let location = data.features[i];
-console.log(location.geometry.coordinates)
+
+        
     // Check for the location property.
         if (location) {
 
       // Add a new marker to the cluster group, and bind a popup.
-        L.marker([location.geometry.coordinates[1], location.geometry.coordinates[0]]).addTo(myMap);
+      L.circle([location.geometry.coordinates[1], location.geometry.coordinates[0]], (location.properties.mag*100000)).addTo(myMap);
         
     
 
@@ -33,6 +37,6 @@ console.log(location.geometry.coordinates)
   
  
 
-    
+   //radius: magnitude 
 
   //.bindPopup([data[i].properties.mag, location.coordinates[2]])
